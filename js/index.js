6bde182292402143685d513df9b2bf0fc1fe42a7
emmahackett <hello@emmahackett-design.com>
2021-02-09 18:50:27 +1030

    Homework 19

---
+++ js/index.js
@@ -0,0 +1,138 @@
+/*-----PRIMARY LINKS------*/
+
+/*INSTAGRAM_BTN*/
+
+$("#instagram-link").hover(function(){
+  $("#instagram-link-line").css("width", "60px");
+});
+
+$("#instagram-link").mouseout(function(){
+  $("#instagram-link-line").css("width", "20px");
+});
+
+$("#instagram-link-mobile").hover(function(){
+  $("#instagram-link-line-mobile").css("width", "60px");
+});
+
+$("#instagram-link-mobile").mouseout(function(){
+  $("#instagram-link-line-mobile").css("width", "20px");
+});
+
+/*LINKEDIN_BTN*/
+
+$("#linkedin-link").hover(function(){
+  $("#linkedin-link-line").css("width", "60px");
+});
+
+$("#linkedin-link").mouseout(function(){
+  $("#linkedin-link-line").css("width", "20px");
+});
+
+$("#linkedin-link-mobile").hover(function(){
+  $("#linkedin-link-line-mobile").css("width", "60px");
+});
+
+$("#linkedin-link-mobile").mouseout(function(){
+  $("#linkedin-link-line-mobile").css("width", "20px");
+});
+
+/*BEHANCE_BTN*/
+
+$("#behance-link").hover(function(){
+  $("#behance-link-line").css("width", "60px");
+});
+
+$("#behance-link").mouseout(function(){
+  $("#behance-link-line").css("width", "20px");
+});
+
+$("#behance-link-mobile").hover(function(){
+  $("#behance-link-line-mobile").css("width", "60px");
+});
+
+$("#behance-link-mobile").mouseout(function(){
+  $("#behance-link-line-mobile").css("width", "20px");
+});
+
+
+/*TRAGENDA_HOME*/
+
+$("#tragenda-link").hover(function(){
+  $("#tragenda-link-line").css("width", "60px");
+});
+
+$("#tragenda-link").mouseout(function(){
+  $("#tragenda-link-line").css("width", "20px");
+});
+
+/*FRIGO_HOME*/
+
+$("#frigo-link").hover(function(){
+  $("#frigo-link-line").css("width", "60px");
+});
+
+$("#frigo-link").mouseout(function(){
+  $("#frigo-link-line").css("width", "20px");
+});
+
+/*TT_HOME*/
+
+$("#tt-link").hover(function(){
+  $("#tt-link-line").css("width", "60px");
+});
+
+$("#tt-link").mouseout(function(){
+  $("#tt-link-line").css("width", "20px");
+});
+
+/*-----PRIMARY BUTTONS------*/
+
+/*TRAGENDA_HOME*/
+
+$("#tragenda-button").hover(function(){
+  $("#tragenda-button-line").css("width", "60px");
+});
+
+$("#tragenda-button").mouseout(function(){
+  $("#tragenda-button-line").css("width", "20px");
+});
+
+/*FRIGO_HOME*/
+
+$("#frigo-button").hover(function(){
+  $("#frigo-button-line").css("width", "60px");
+});
+
+$("#frigo-button").mouseout(function(){
+  $("#frigo-button-line").css("width", "20px");
+});
+
+/*TT_HOME*/
+
+$("#tt-button").hover(function(){
+  $("#tt-button-line").css("width", "60px");
+});
+
+$("#tt-button").mouseout(function(){
+  $("#tt-button-line").css("width", "20px");
+});
+
+/*LEARN MORE_BUTTON*/
+
+$("#about-button").hover(function(){
+  $("#about-button-line").css("width", "60px");
+});
+
+$("#about-button").mouseout(function(){
+  $("#about-button-line").css("width", "20px");
+});
+
+/*BACK_BUTTON*/
+
+$("#back-button").hover(function(){
+  $("#back-button-line").css("width", "60px");
+});
+
+$("#back-button").mouseout(function(){
+  $("#back-button-line").css("width", "20px");
+});
